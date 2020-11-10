b7faefe4d3be0261128396bf9cbf9ab2cf1001e4
kevin4845 <madsen070602@hotmail.com>
2020-11-10 08:25:41 +0100

    Initialized All Files

---
+++ script.js
@@ -0,0 +1,17 @@
+function Roulette(userChoice) {
+    var pcChoice = Math.floor((Math.random() * 3) + 1);
+
+    if (pcChoice == 1) {
+        
+    }
+
+    if (pcChoice == 2) {
+        
+    }
+
+    if (pcChoice == 3) {
+        
+    }
+    
+
+}
